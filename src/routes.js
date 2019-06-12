ecfcdfc4b7e6d455f1118527d9d511a088199f47
fogelman <ddfogelman@gmail.com>
2019-06-12 02:03:40 -0300

    adding post

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 const controllers = require('./app/controllers');
 
 routes.get('/log', controllers.LogController.show);
-routes.get('/', controllers.LogController.store);
+routes.post('/', controllers.LogController.store);
 routes.delete('/log', controllers.LogController.delete);
 
 module.exports = routes;
